da5c3fd4ba03d0a010c514f68ada4d150e077194
tzimar <275237023+tzimar@users.noreply.github.com>
2026-05-16 21:25:33 +1000

    Refactor script; add options

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Gitscribe
 
-Gitscribe is a simple utility that creates a local Git repository, polls the working directory for changes, and automatically commits them. It's intended for use with simple, text-based files like manuscripts.
+Gitscribe is a simple utility that polls the working directory for changes in a Git repo, and automatically commits them. It's intended for use with simple, text-based files like manuscripts.
 
 ## Distribution
 
--- gitscribe.sh
+++ gitscribe.sh
@@ -1,51 +1,103 @@
 version="1.0.0"
 
-main(){
-    if [ ! -d .git ]; then
-        git init
+Help(){
+    echo "usage: gitscribe.sh [-h] [-n] [-d directory] [-f frequency]"
+    echo
+    echo "h     Help"
+    echo "n     Create repository"
+    echo "d     Set working directory"
+    echo "f     Polling frequency (seconds)"
+}
 
-        git config --global --add safe.directory $(pwd -P)
+InitRepo(){
+    git init
 
-        # unpack archive
-        echo "$archive" | base64 -d > .gitscribe.tar
-        tar -xvf .gitscribe.tar
-        rm .gitscribe.tar
-        regex='dot(\..*)'
-        for f in dot.*; do
-            [[ $f =~ $regex ]] && mv "$f" "${BASH_REMATCH[1]}"
+    git config --global --add safe.directory $(pwd -P)
+
+    # unpack archive
+    echo "$archive" | base64 -d > .gitscribe.tar
+    tar -xvf .gitscribe.tar
+    rm .gitscribe.tar
+    regex='dot(\..*)'
+    for f in dot.*; do
+        [[ $f =~ $regex ]] && mv "$f" "${BASH_REMATCH[1]}"
+    done
+    
+    git commit --quiet --allow-empty --allow-empty-message -m "(root commit)"
+}
+
+CheckRepo(){
+    msg=""
+
+    new_files=($(git ls-files --others --exclude-standard))
+    mod_files=($(git diff-index --name-only HEAD --))
+
+    if [[ -n $new_files || -n $mod_files ]]; then
+
+        # add new line to file endings
+        all_files=( "${new_files[@]}" "${mod_files[@]}" )
+        for file in "${all_files[@]}"; do
+            sed -i '$a\' $file
+            sed -i 's/$/\r/' $file
         done
 
-        git config --local include.path ../.gitconfig
-        
-        git commit --quiet --allow-empty --allow-empty-message -m "(root commit)"
+        git add .
+        if [[ -n "$(git status -s)" ]]; then
+            git commit --quiet --allow-empty-message -m ""
+        fi
+    fi
+}
 
+Main(){
+
+    dir=$PWD
+    init_repo=false
+    freq=1
+
+    while getopts "hnd:f:" opt; do
+        case $opt in
+            h)
+                Help
+                exit
+                ;;
+            n)
+                init_repo=true
+                ;;
+            d)
+                dir=$(realpath "$dir/$OPTARG")
+                ;;
+            f)
+                freq=$OPTARG
+                ;;
+           \?)
+                echo "Invalid option."
+                exit
+                ;;
+        esac
+    done
+
+    if [[ ! -d "$dir" ]]; then
+        echo "Directory does not exist."
+        exit
+    fi
+    cd "$dir"
+
+    if $init_repo; then
+        if [ -d .git ]; then
+            echo "A repository already exists."
+            exit
+        fi
+        InitRepo
+    elif [ ! -d .git ]; then
+        echo "No repository in this directory."
+        exit
     fi
 
-    while sleep 1; do
-
-        msg=""
-
-        new_files=($(git ls-files --others --exclude-standard))
-        mod_files=($(git diff-index --name-only HEAD --))
-
-        if [[ -n $new_files || -n $mod_files ]]; then
-
-            # add new line to file endings
-            all_files=( "${new_files[@]}" "${mod_files[@]}" )
-            for file in "${all_files[@]}"; do
-                sed -i '$a\' $file
-                sed -i 's/$/\r/' $file
-            done
-
-            git add .
-            if [[ -n "$(git status -s)" ]]; then
-                git commit --quiet --allow-empty-message -m ""
-            fi
-        fi
-
+    while sleep $freq; do
+        CheckRepo
     done
 }
 
 archive=""
 
-main
+Main "$@"
